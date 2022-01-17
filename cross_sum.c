8b1ac67565b22c140003e73d21ddc0cd7622d75d
PmpHtt <97919987+PmpHtt@users.noreply.github.com>
2022-01-17 23:03:04 +0100

    Erster Commit, Cross Sum eingefügt

---
+++ cross_sum.c
@@ -0,0 +1,14 @@
+ 
+int qs(int n){
+    int helpvar = n;
+    int quers = 0;
+    int stelle = 10;
+    while (helpvar > 0){
+        quers += (helpvar%stelle);
+        helpvar -= (helpvar%stelle);
+        helpvar /= 10;
+
+    }
+    return quers;
+
+}
